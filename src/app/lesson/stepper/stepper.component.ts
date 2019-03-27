eea884135c7dea7a46be59389e735e725a853205
wandererparsifal <wandererparsifal@gmail.com>
2019-03-27 18:10:07 +0800

    修改 optional 文字

--- src/app/lesson/stepper/stepper.component.ts
+++ src/app/lesson/stepper/stepper.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatStepperIntl } from '@angular/material';
+
+export class TwStepperIntl extends MatStepperIntl {
+  optionalLabel = '非必填';
+}
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.css']
+  styleUrls: ['./stepper.component.css'],
+  providers: [{ provide: MatStepperIntl, useClass: TwStepperIntl }]
 })
 export class StepperComponent implements OnInit {
 
