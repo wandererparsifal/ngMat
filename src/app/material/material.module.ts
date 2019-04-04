978bf8203d5d595fc033b53e16ae56eae59858d6
wandererparsifal <wandererparsifal@gmail.com>
2019-04-04 14:42:58 +0800

    自定义显示格式

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {
+  MAT_DATE_FORMATS,
   MAT_DATE_LOCALE,
   MatAutocompleteModule,
   MatButtonModule,
@@ -17,9 +18,24 @@ import {
 } from '@angular/material';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
+export const CN_FORMATS = {
+  parse: {
+    dateInput: 'YYYY/MM/DD'
+  },
+  display: {
+    dateInput: 'YYYY/MM/DD',
+    monthYearLabel: 'YYYY MMM',
+    dateA11yLabel: 'YYYY/MM/DD',
+    monthYearA11yLabel: 'YYYY MMM'
+  }
+};
+
 @NgModule({
   declarations: [],
-  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'zh-CN' }],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'zh-CN' },
+    { provide: MAT_DATE_FORMATS, useValue: CN_FORMATS }
+  ],
   imports: [
     CommonModule,
     MatButtonModule,
