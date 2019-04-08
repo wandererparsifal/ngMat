dd8ac4e4ae5bb54f26cb38e93eb4e075886f275f
wandererparsifal <wandererparsifal@gmail.com>
2019-04-08 15:41:03 +0800

    mat-checkbox indeterminate 状态

--- src/app/lesson/selection/selection.component.ts
+++ src/app/lesson/selection/selection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material';
 
 @Component({
   selector: 'app-selection',
@@ -10,15 +11,44 @@ export class SelectionComponent implements OnInit {
 
   mainQuestions: FormGroup;
 
+  indeterminateSelectedPayFor: boolean;
+
   constructor() {
     this.mainQuestions = new FormGroup({
       payForBook: new FormControl(false),
       payForMusic: new FormControl(false),
       payForMovie: new FormControl(true),
+      payForAll: new FormControl(true),
     });
   }
 
   ngOnInit() {
+    this._setSelectAllState();
   }
 
+  checkAllChange($event: MatCheckboxChange) {
+    this.mainQuestions
+      .get('payForBook')
+      .setValue($event.checked);
+    this.mainQuestions
+      .get('payForMusic')
+      .setValue($event.checked);
+    this.mainQuestions
+      .get('payForMovie')
+      .setValue($event.checked);
+    this._setSelectAllState();
+  }
+
+  payForChange() {
+    this._setSelectAllState();
+  }
+
+  private _setSelectAllState() {
+    const payForBook = this.mainQuestions.get('payForBook').value;
+    const payForMusic = this.mainQuestions.get('payForMusic').value;
+    const payForMovie = this.mainQuestions.get('payForMovie').value;
+    const count = (payForBook ? 1 : 0) + (payForMusic ? 1 : 0) + (payForMovie ? 1 : 0);
+    this.mainQuestions.get('payForAll').setValue(count === 3);
+    this.indeterminateSelectedPayFor = count > 0 && count < 3;
+  }
 }
